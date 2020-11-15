ac8220ac61757babafbc02ebdbeb453615e5b68f
MariiaAmetina <70066337+MariiaAmetina@users.noreply.github.com>
2020-11-15 18:59:06 +0300

    react files

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+function App() {
+  return <div>
+    <p>Hello</p> 
+    <p>My</p> 
+    <p>Darling</p> 
+    <input placeholder='type smth'/> <br />
+    <input placeholder='type smth else'/> <br />
+    <input placeholder='type smth more'/> <br />
+
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
+
+    <table>
+      <tBody>
+        <tr>
+          <td>z</td>
+          <td>z</td>
+          <td>z</td>
+        </tr>
+        <tr>
+          <td>z</td>
+          <td>z</td>
+          <td>z</td>
+        </tr>
+        <tr>
+          <td>z</td>
+          <td>z</td>
+          <td>z</td>
+        </tr>
+      </tBody>
+    </table>
+
+    <p className="red">red</p>
+    <p className="pink">pink</p>
+    <p className="blue">blue</p>
+  </div>;
+}
+
+export default App;
